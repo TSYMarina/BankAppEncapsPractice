96c6de01d045b4e3e2b4a0daee1037f3896395e0
Marina Tsymlianskaia <marinatsy16@gmail.com>
2022-02-22 11:21:29 -0700

    * .gitignore: first commit * BankAppEncapsPractice.sln: * BankAppEncapsPractice/Program.cs: * BankAppEncapsPractice/BankAccount.cs: * BankAppEncapsPractice/BankAppEncapsPractice.csproj:

---
+++ BankAppEncapsPractice/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace BankAppEncapsPractice
+{
+    public class BankAccount
+    {
+        private double _balance = 0; // private is teh default access modifier for fields in classes; use underscore for private field names
+
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+        public double GetBalance()
+        {
+        return _balance;
+        }
+
+    }
+}
---
+++ BankAppEncapsPractice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BankAppEncapsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount checkingAccount = new BankAccount();
+                      
+
+            Console.WriteLine($"Please enter the amount you would like to deposit: ");
+
+            double amount = 0.0;
+
+            bool validDeposit = false;
+
+            while (!validDeposit)
+            {
+                if(double.TryParse(Console.ReadLine(), out amount))
+                {
+                    validDeposit = true;
+
+                }
+                else
+                {
+                    Console.WriteLine($"Please enter a valid ammount!");
+                }
+            }
+
+            checkingAccount.Deposit(amount);
+
+            Console.WriteLine($"Your Balance is now: {checkingAccount.GetBalance()}");
+
+        }
+
+    }
+}
